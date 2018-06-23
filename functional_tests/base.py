1576a193ee33abb47a3775a064f79453314323a1
Joachim Hagege <cyberjoac@gmail.com>
2018-06-23 23:49:59 +0300

    Management.py against server

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,6 +4,8 @@ from selenium.common.exceptions import WebDriverException
 import time
 import os
 
+from .server_tools import reset_database
+
 MAX_WAIT = 10
 
 
@@ -14,6 +16,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get('STAGING_SERVER')
         if self.staging_server:
             self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         self.browser.quit()
